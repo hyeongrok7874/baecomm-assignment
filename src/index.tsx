73b5793b22334adecdfc765b8fffe3151c48dde5
hyeongrok7874 <khr5k0228@gmail.com>
2024-04-12 13:54:18 +0900

    Add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "@/styles";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+const color = {
+  blue: "#5BBCFF",
+  yellow: "#FFFAB7",
+  pink: "#FFD1E3",
+};
+
+const theme = {
+  color,
+};
+
+export default theme;
